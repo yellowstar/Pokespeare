4fde4f774a5611e0ec8741cba0d271f3dfebf4f4
Rupert Hollom <rupert@hollom.com>
2020-11-26 21:30:14 +0000

    Initial creation of API Service

---
+++ Pokespeare/Controllers/PokemonController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Pokespeare.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class PokemonController : ControllerBase
+	{
+		private readonly ILogger<PokemonController> _logger;
+
+		public PokemonController(ILogger<PokemonController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet("{pokemonName}/{translation?}")]
+		public IActionResult Index([FromRoute(Name = "pokemonName")] string PokemonName, [FromRoute(Name = "translation")] string Translation)
+		{
+			return Ok();
+		}
+	}
+}
